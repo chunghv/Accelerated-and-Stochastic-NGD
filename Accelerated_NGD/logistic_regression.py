8d05ae8a65baa187b97cc669e581500780b12b8e
chunghoangvan44-droid <chunghoangvan44@gmail.com>
2026-02-21 21:53:19 +0700

    add more data

--- Accelerated_NGD/logistic_regression.py
+++ Accelerated_NGD/logistic_regression.py
@@ -6,26 +6,47 @@ import matplotlib.pyplot as plt
 import numpy.linalg as la
 from sklearn.datasets import load_svmlight_file
 
-from optimizers import Gd, Nesterov, Adgd, NGD, NGDn, NGDh, AdgdAccel
+from optimizers import *
 from loss_functions import logistic_loss, logistic_gradient
+
 import time
 
 sns.set(style="whitegrid", font_scale=1.2, context="talk", palette=sns.color_palette("bright"), color_codes=False)
 matplotlib.rcParams['mathtext.fontset'] = 'cm'
-dataset = 'cod-rna.txt' # 'phishing.txt'  'a9a'  'w8a' 'cod-rna'
+dataset = 'mushrooms' 
+dataset = 'w8a' 
+dataset = 'a9a'  
+dataset = 'phishing' 
+dataset = 'cod-rna' 
+dataset = 'ijcnn1'
+dataset = 'skin_nonskin'
+
+
 data_path = './datasets/' + dataset
 if dataset == 'covtype':
     data_path += '.bz2'
 
+if dataset == 'ijcnn1' or dataset == 'rcv1' or dataset == 'real-sim' or dataset == 'imdb_sentiment':
+    data_path += '.bz2'
+
 if dataset == 'covtype':
-    it_max = 10000
-else:
-    # it_max = 3000
     it_max = 1000
+    
+elif dataset == 'mushrooms':
+    it_max = 3000
+elif dataset == 'w8a':
+    it_max = 2000
+elif dataset == 'a9a':
+    it_max = 1000
+elif dataset == 'phishing':
+    it_max = 2000
+elif dataset == 'cod-rna':
+    it_max = 1000
+else:
+    it_max = 3000
 
 def logistic_smoothness(X):
     return 0.25 * np.max(la.eigvalsh(X.T @ X / X.shape[0]))
-
 
 data = load_svmlight_file(data_path)
 X, y = data[0].toarray(), data[1]
@@ -36,48 +57,78 @@ if (np.unique(y) == [-1, 1]).all():
     y = (y+1) / 2
 n, d = X.shape
 L = logistic_smoothness(X)
-l = L / n if dataset == 'covtype' else L / (10 * n) 
+l2 = L / n if dataset == 'covtype' else L / (10 * n) 
 w0 = np.zeros(d)
 
 def loss_func(w):
-    return logistic_loss(w, X, y, l)
+    return logistic_loss(w, X, y, l2)
 
 def grad_func(w):
-    return logistic_gradient(w, X, y, l)
+    return logistic_gradient(w, X, y, l2)
 
+print(X.shape)
+
+print("Running Gd...")
 start_time = time.time()
 gd = Gd(lr=1 / L, loss_func=loss_func, grad_func=grad_func, it_max=it_max)
 gd.run(w0=w0)
-print("Gd --- %s seconds ---" % (time.time() - start_time))
+print("--- %s seconds ---" % (time.time() - start_time))
 
+print("Running Adgd...")
 start_time = time.time()
 adgd = Adgd(loss_func=loss_func, grad_func=grad_func, eps=0, it_max=it_max)
 adgd.run(w0=w0)
-print("Adgd --- %s seconds ---" % (time.time() - start_time))
+print("--- %s seconds ---" % (time.time() - start_time))
 
+print("Running AdgdAccel...")
 start_time = time.time()
 ad_acc = AdgdAccel(loss_func=loss_func, grad_func=grad_func, it_max=it_max)
 ad_acc.run(w0=w0)
-print("AdgdAccel --- %s seconds ---" % (time.time() - start_time))
+print("--- %s seconds ---" % (time.time() - start_time))
 
+print("Running NGD...")
 start_time = time.time()
 ngd = NGD(loss_func=loss_func, grad_func=grad_func, lr0=1e-3, eta0=0.2, eta1=0.15, beta=4.5, alpha=2, it_max=it_max)
 ngd.run(w0=w0)
-print("NGD --- %s seconds ---" % (time.time() - start_time))
+print("--- %s seconds ---" % (time.time() - start_time))
 
+print("Running NGDn...")
 start_time = time.time()
-ngdn = NGDn(loss_func=loss_func, grad_func=grad_func, lr0=1e-3, eta0=0.2, eta1=0.19, beta=0, alpha=3.0, it_max=it_max)
+ngdn = NGDn(loss_func=loss_func, grad_func=grad_func, lr0=0.01, eta0=0.2, eta1=0.19, beta=0, alpha=3.0, gamma = 0.9, it_max=it_max)
 ngdn.run(w0=w0)
-print("NGDn --- %s seconds ---" % (time.time() - start_time))
+print("--- %s seconds ---" % (time.time() - start_time))
 
+print("Running NGDh...")
 start_time = time.time()
-ngdh = NGDh(loss_func=loss_func, grad_func=grad_func, lr0=1e-3, eta0=0.2, eta1=0.19, beta=0, alpha=3.0, it_max=it_max)
+ngdh = NGDh(loss_func=loss_func, grad_func=grad_func, lr0=0.01, eta0=0.2, eta1=0.19, beta=0, alpha=3.0, gamma = 0.9, it_max=it_max)
 ngdh.run(w0=w0)
-print("NGDh --- %s seconds ---" % (time.time() - start_time))
+print("--- %s seconds ---" % (time.time() - start_time))
 
-optimizers = [gd, adgd, ad_acc, ngd, ngdn, ngdh]
-markers = [',', 'o', '^', '*', 'D', 'x']
-labels = ['GD', 'AdGD', 'AdGD-accel', 'NGD','NGDn', 'NGDh']
+
+print("Running Nesterov...")
+start_time = time.time()
+nesterov = Nesterov(lr=1 / L, strongly_convex=True, mu=l2, loss_func=loss_func, grad_func=grad_func, it_max=it_max)
+nesterov.run(w0=w0)
+print("--- Nesterov Time: %s seconds ---" % (time.time() - start_time))
+
+print("Running Heavy Ball...")
+start_time = time.time()
+heavy_ball = HeavyBall(
+    L=L, 
+    mu=l2, 
+    loss_func=loss_func, 
+    grad_func=grad_func, 
+    it_max=it_max
+)
+
+heavy_ball.run(w0=w0.copy())
+
+print("--- Heavy Ball Time: %s seconds ---" % (time.time() - start_time))
+
+
+optimizers = [gd, adgd, ad_acc, ngd, ngdn, ngdh, nesterov, heavy_ball]
+markers = [',', 'o', '^', '*', 'D', 'x', 's', 'v']
+labels = ['GD', 'AdGD', 'AdGD-accel', 'NGD','NGDn', 'NGDh', 'Nesterov-accel', "Heavy-ball-accel"]
 
 for opt, marker in zip(optimizers, markers):
     opt.compute_loss_on_iterates()
@@ -91,5 +142,7 @@ plt.xlabel('Iteration')
 plt.ylabel(r'$f(x^k) - f_*$')
 plt.legend()
 plt.tight_layout()
-plt.show()
-plt.savefig(f'results/{dataset}.pdf', bbox_inches='tight', dpi=300)
+
+# plt.savefig("skin_nonskin.pdf", format="pdf", bbox_inches="tight")
+
+plt.close()  
